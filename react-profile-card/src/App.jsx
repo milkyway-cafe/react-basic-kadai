11ddf1355239fb52835e19022fd7a653983a8503
milkyway-cafe <rainbowpastelflower@gmail.com>
2025-11-30 23:36:50 +0900

    edited upload

---
+++ react-profile-card/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import  ProfileCard  from './ProfileCard';
+
+function App() {
+  return (
+    <>
+    <ProfileCard />
+    </>
+  )
+}
+
+export default App
